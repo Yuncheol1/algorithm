5e981e8cb93ea4366f858c1dd349a6127bba56b3
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2024-04-29 15:04:42 +0900

    [Gold V] Title: 강의실, Time: 48 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+
+	vector <pair<int, int>> study;
+	int n;
+	cin >> n;
+	int start, end,what;
+	for (int i = 0; i < n; i++) {
+		cin >> what>>start >> end;
+		study.push_back(make_pair(start, 1));
+		study.push_back(make_pair(end, -1));
+	}
+	sort(study.begin(), study.end());
+	int sum = 0;
+	int max = 0;
+	for (int i = 0; i < study.size(); i++) {
+		sum += study[i].second;
+		if (max < sum) {
+			max = sum;
+		}
+	}
+	cout << max;
+
+
+	return 0;
+}
