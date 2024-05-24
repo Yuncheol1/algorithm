67634518e10c06ae16cb5447f0794e7b0c08f647
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2024-05-24 15:48:15 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <stack>
+#include <queue>
+#include <functional>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int n;
+    int num;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        if (num == 0) {
+            if (pq.empty())cout << 0 << "\n";
+            else {
+                cout <<pq.top() << '\n';
+                pq.pop();
+            }continue;
+        }
+        pq.push(num);
+       
+    }
+
+
+
+    return 0;
+}
