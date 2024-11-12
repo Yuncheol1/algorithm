8b3edc9c9c0bb04b23f623e5a654e811116229a7
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2024-11-12 15:41:56 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 276 ms, Memory: 40528 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 1 || n == 2) {
+            System.out.println(2);
+            return;
+        }
+        if (n % 2 == 0) {
+            n++;
+        }
+        for (int i = n; ; i += 2) {
+            int j;
+            boolean check = true;
+            String str = Integer.toString(i);
+            int left = 0;
+            int right = str.length() - 1;
+
+            while (left < right) {
+                if (str.charAt(left) != str.charAt(right)) {
+                    check = false;
+                    break;
+                } else {
+                    left++;
+                    right--;
+                }
+            }
+
+            if (check) {
+                boolean check2 = true;
+                for (j = 2; j <= (int) Math.sqrt(i); j++) {
+                    if (i % j == 0) {
+                        check2 = false;
+                        break;
+                    }
+                }
+                if (check2) {
+                    System.out.print(i);
+                    return;
+                }
+            }
+        }
+
+    }
+}
