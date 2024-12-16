d00d5c469859b34af1853f472040048179f060f6
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2024-12-16 10:13:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 1388 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.c
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.c
@@ -2,30 +2,25 @@
 #include <stdlib.h>
 #pragma warning(disable:4996)
 #define max(x, y) (((x) > (y)) ? (x) : (y))
-int dp[101][1000001];
-int w[101];
-int v[101];
-int n, k;
+
 int main() {
-	
+	int n, k;
+	int v[101];
+	int w[101];
+	int dp[100001] = { 0 };
 	scanf("%d %d", &n, &k);
 	
 	for (int i = 1; i <= n; i++) {
 		scanf("%d %d", &w[i], &v[i]);
 	}
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (j - w[i] >= 0) {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-			}
-			else {
-				dp[i][j] = dp[i - 1][j];
+		for (int j = k; j >= 1; j--) {//현재 가방의 무게
+			if (j >= w[i]) {
+				dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
 			}
 		}
 	}
-	printf("%d", dp[n][k]);
-
-
+	printf("%d", dp[k]);
 
 	return 0;
 }
