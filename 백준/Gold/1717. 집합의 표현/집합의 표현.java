299636e0679ae651c8ebdec2d60ab46ebac6eeb2
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2024-12-30 08:55:00 +0900

    [Gold V] Title: 집합의 표현, Time: 928 ms, Memory: 53584 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,51 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
 import java.util.StringTokenizer;
+
 public class Main {
-    static int[]x;
-    static void union(int a,int b){
-        a=find(a);
-        b=find(b);
-        if(a!=b)x[b]=a;
+    static int[] ans;
+
+    static void union(int a, int b) {
+        int A = find(a);
+        int B = find(b);
+        if (A < B) {
+            ans[B] = A;
+        } else if (A > B) {
+            ans[A] = B;
+        }
     }
+
     static int find(int a) {
-        if (a == x[a]){
+        if (a == ans[a]) {
             return a;
-        }
-        else{
-            return x[a]=find(x[a]);
+        } else {
+            return ans[a] = find(ans[a]);
         }
     }
-    static boolean same(int a,int b){
-        int ans1=find(a);
-        int ans2=find(b);
-        if(ans1==ans2)return true;
-        else return false;
+
+    static boolean checksame(int a, int b) {
+        int A = find(a);
+        int B = find(b);
+        if (A == B) {
+            return true;
+        }
+        return false;
     }
+
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        x=new int[n+1];
-        for(int i=0;i<n+1;i++)x[i]=i;
-        for(int i=0;i<m;i++){
-            int q=sc.nextInt();
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-
-            if(q==0){
-                union(a,b);
-            }
-            else{
-
-               if(same(a,b))System.out.println("yes");
-               else System.out.println("no");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int q = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (q == 0) {
+                union(a, b);
+            } else {
+                if (checksame(a, b)) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
             }
         }
 
     }
 }
+
+
+
+
