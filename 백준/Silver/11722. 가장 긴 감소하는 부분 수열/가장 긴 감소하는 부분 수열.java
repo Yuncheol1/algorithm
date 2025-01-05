f56827d01a1cc7d034204ceeee4ec6b514f055b8
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-01-05 15:21:58 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 248 ms, Memory: 21052 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            x[i] = sc.nextInt();
+        }
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 1);
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if (x[i] < x[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        Arrays.sort(dp);
+        System.out.println(dp[n]);
+    }
+
+}
+
+
