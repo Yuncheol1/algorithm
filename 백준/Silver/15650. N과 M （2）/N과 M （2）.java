69b1f231746b4b99d684bb0c16ed54e4d31882ae
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-01-09 20:01:42 +0900

    [Silver III] Title: N과 M (2), Time: 180 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,38 +1,38 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static int[] arr;
 
-    public static int[] arr;
-    public static int N, M;
-
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-
-        N = in.nextInt();
-        M = in.nextInt();
-
-        arr = new int[M];
-
-        dfs(1, 0);
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        visited = new boolean[n];
+        arr = new int[m];
+        dfs(0, 0);
+        System.out.println(sb);
 
     }
 
-    public static void dfs(int at, int depth) {
-
-        if (depth == M) {
-            for (int val : arr) {
-                System.out.print(val + " ");
+    static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
-
-        for (int i = at; i <= N; i++) {
-
-            arr[depth] = i;
-            dfs(i + 1, depth + 1);
-
+        for (int i = start; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1, i + 1);
+                visited[i] = false;
+            }
         }
     }
 }
