ac98ce7e20ec6d4410ee450e77b06347a1ca907a
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-01-09 20:13:43 +0900

    [Silver III] Title: N과 M (4), Time: 216 ms, Memory: 20816 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        visited = new boolean[n];
+        arr = new int[m];
+        dfs(0);
+        System.out.println(sb);
+
+    }
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (depth == 0) {
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+            }
+            if (depth >= 1 && arr[depth - 1] <= i + 1) {
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+            }
+        }
+    }
+}
