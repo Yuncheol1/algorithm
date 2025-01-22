111cf600d8f507ca8dcdb55110a15ccfdb9aa9be
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-01-22 21:50:48 +0900

    [Silver III] Title: N과 M (6), Time: 176 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static int[] ans;
+    static int[] x;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        x = new int[n + 1];
+        visited = new boolean[n + 1];
+        ans = new int[m];
+        for (int i = 1; i <= n; i++) {
+            x[i] = sc.nextInt();
+        }
+        Arrays.sort(x);
+        BackTracking(0,1);
+        System.out.println(sb);
+    }
+
+    static void BackTracking(int depth,int start) {
+        if (depth == m) {
+            for (int i : ans) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                ans[depth] = x[i];
+                BackTracking(depth + 1,i);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
+
