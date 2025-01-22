e9d825c8e077d2d8279be29d2bedadb152f53b9b
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-01-22 21:40:51 +0900

    [Silver III] Title: N과 M (2), Time: 172 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,38 +1,42 @@
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     static int n, m;
     static StringBuilder sb = new StringBuilder();
     static boolean[] visited;
-    static int[] arr;
+    static int[] ans;
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        visited = new boolean[n];
-        arr = new int[m];
-        dfs(0, 0);
+        visited = new boolean[n + 1];
+        ans = new int[m];
+        BackTracking(0,1);
         System.out.println(sb);
-
     }
 
-    static void dfs(int depth, int start) {
+    static void BackTracking(int depth,int start) {
         if (depth == m) {
-            for (int i : arr) {
+            for (int i : ans) {
                 sb.append(i).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for (int i = start; i < n; i++) {
+
+        for (int i = start; i <= n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                arr[depth] = i + 1;
-                dfs(depth + 1, i + 1);
+                ans[depth] = i;
+                BackTracking(depth + 1,i+1);
                 visited[i] = false;
             }
         }
+
+
     }
+
 }
