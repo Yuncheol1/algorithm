12394a9f2c6776480db1dcdfe10bb480e7a00eae
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-01-31 15:42:20 +0900

    [Silver III] Title: 01타일, Time: 184 ms, Memory: 21640 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 1 || n == 2) {
+            System.out.println(n);
+            return;
+        }
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+        System.out.println(dp[n]);
+    }
+
+}
