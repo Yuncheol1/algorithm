b7b1213bb95648ef6d5f23c785fc97cb412fd6c0
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-02-11 00:44:23 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 624 ms, Memory: 141668 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,64 +1,48 @@
-//package algorithm;
-import java.io.*;
-import java.util.*;
-import java.util.Scanner;
-class graph{
-    int num;
-    ArrayList<LinkedList<Integer>> adj;
-boolean[] visited;
-    public graph(int num){
-    this.num=num;
-    adj=new ArrayList<>(num);
-    visited=new boolean[num];
-    for(int i=0;i<num;i++)
-     adj.add(new LinkedList<>());
-  }
-void addEdge(int v,int e){
-adj.get(v).add(e);
-adj.get(e).add(v);
-}
-void dfs(int vertex){
-        visited[vertex]=true;
-   //     System.out.print(vertex);
-        for(int i: adj.get(vertex)){
-            if(!visited[i]){
-                dfs(i);
-            }
-        }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-}
-    public void printGraph() {
-        for (int i = 0; i < num; i++) {
-            System.out.print("Vertex " + i + ": ");
-            for (Integer neighbor : adj.get(i)) {
-                System.out.print(neighbor + " ");
-            }
-            System.out.println();
-        }
-    }
-}
 public class Main {
+    static ArrayList<Integer>[] arr;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
-       BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st=new StringTokenizer(br.readLine());
-        int n,m,v,e;
-       n=Integer.parseInt(st.nextToken());
-       m=Integer.parseInt(st.nextToken());
-        graph g=new graph(n+1);
-        for(int i=0;i<m;i++){
-            st=new StringTokenizer(br.readLine());
-            v=Integer.parseInt(st.nextToken());
-            e=Integer.parseInt(st.nextToken());
-            g.addEdge(v,e);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            arr[i] = new ArrayList<>();
         }
-        int cnt=0;
-        for(int i=1;i<n+1;i++){
-            if(!g.visited[i]){
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a].add(b);
+            arr[b].add(a);
+        }
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i);
                 cnt++;
-                g.dfs(i);
             }
         }
-        System.out.print(cnt);
+        System.out.println(cnt);
     }
+
+    static void dfs(int start) {
+        if (visited[start]) {
+            return;
+        }
+        visited[start] = true;
+        for (int i : arr[start]) {
+            dfs(i);
+        }
+    }
+
 }
