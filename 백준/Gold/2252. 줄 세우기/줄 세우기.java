8f0a78cee5417e156bb3e73e30f311ea24b6c99a
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-03-23 03:10:22 +0900

    [Gold III] Title: 줄 세우기, Time: 1312 ms, Memory: 174204 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,41 +1,38 @@
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static ArrayList<ArrayList<Integer>> st = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[] indegree = new int[n + 1];
+        int[] x=new int[n+1];
+        ArrayList<ArrayList<Integer>> tall = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
-            st.add(new ArrayList<>());
+            tall.add(new ArrayList<>());
         }
-        for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            st.get(a).add(b);
-            indegree[b]++;
+        for(int i=0;i<m;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            tall.get(a).add(b);
+            x[b]++;
         }
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 1; i <= n; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=1;i<=n;i++){
+            if(x[i]==0){
+                q.add(i);
             }
         }
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            System.out.print(now + " ");
-            for (int next : st.get(now)) {
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    q.offer(next);
-                }
+        while(!q.isEmpty()){
+            int now=q.poll();
+            System.out.print(now+" ");
+            for( var t:tall.get(now)){
+            x[t]--;
+                if(x[t]==0)q.add(t);
             }
+
         }
+
+
     }
 }
