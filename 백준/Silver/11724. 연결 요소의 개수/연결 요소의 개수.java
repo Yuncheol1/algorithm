10403c708fd1e7a5c6329a30a238504ede4ea7a7
Yuncheol1 <126949603+Yuncheol1@users.noreply.github.com>
2025-07-19 14:28:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 568 ms, Memory: 141708 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,48 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static ArrayList<Integer>[] arr;
-    static boolean[] visited;
-
+    static ArrayList<Integer>[] graph;
+    static ArrayDeque<Integer> stack=new ArrayDeque<>();
+    static boolean[]visited;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        arr = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        for (int i = 0; i <= n; i++) {
-            arr[i] = new ArrayList<>();
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+        graph=new ArrayList[n+1];
+        visited=new boolean[n+1];
+        for(int i=0;i<n+1;i++){
+            graph[i]=new ArrayList<>();
         }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr[a].add(b);
-            arr[b].add(a);
+        for(int i=1;i<=m;i++){
+            st=new StringTokenizer(br.readLine());
+            int u=Integer.parseInt(st.nextToken());
+            int v=Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
         }
-        int cnt = 0;
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                dfs(i);
+        int cnt=0;
+        for(int i=1;i<n+1;i++){
+            if(!visited[i]){
+                stack.push(i);
+                visited[i]=true;
+                dfs();
                 cnt++;
             }
         }
         System.out.println(cnt);
-    }
 
-    static void dfs(int start) {
-        if (visited[start]) {
-            return;
-        }
-        visited[start] = true;
-        for (int i : arr[start]) {
-            dfs(i);
-        }
-    }
 
+    }
+    static void dfs(){
+        while(!stack.isEmpty()){
+            int now=stack.pop();
+            for(int i=0;i<graph[now].size();i++){
+                if(!visited[graph[now].get(i)]){
+                    stack.push(graph[now].get(i));
+                    visited[graph[now].get(i)]=true;
+                }
+            }
+
+
+        }
+
+
+
+
+    }
 }
